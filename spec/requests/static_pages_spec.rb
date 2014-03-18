1291adf4364be901c52a118469f54784dea93b87
Szymon <sz.soppa@gmail.com>
2014-03-18 19:47:24 +0100

    Some test changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,31 +4,57 @@ describe "Static pages" do
 
   subject { page }
 
+  shared_examples_for 'All static pages' do
+  	it { should have_selector('h1', text: heading) }
+  	it { should have_title(full_title(title)) } 
+  end
+
   describe "Home page" do
     before { visit root_path }
+    let(:heading) {'Sample'}
+    let(:title) {''}
 
-    it { should have_content('Sample App') }
+    it { should have_selector('h1', text: 'Sample') }
     it { should have_title(full_title('')) }
   end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading) {'Help'}
+    let(:title) {''}
 
-    it { should have_content('Help') }
+    it { should have_selector('h1', text: 'Help') }
     it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading) {'About'}
+    let(:title) {''}
 
-    it { should have_content('About') }
+    it { should have_selector('h1', text: 'About') }
     it { should have_title(full_title('About Us')) }
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading) {'Contact'}
+    let(:title) {''}
 
-    it { should have_content('Contact') }
+    it { should have_selector('h1', text: 'Contact') }
     it { should have_title(full_title('Contact')) }
   end
+
+  it "should have the right links" do
+  	visit root_path
+  	click_link "About"
+  	expect(page).to have_title(full_title('About Us'))
+  	click_link "Contact"
+  	expect(page).to have_title(full_title('Contact'))
+  	click_link "Help"
+  	expect(page).to have_title(full_title('Help')) 
+  	click_link "Home"
+  	expect(page).to have_title(full_title('Home')) 
+  end
+
 end
